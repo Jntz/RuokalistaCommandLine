8e1309e6e7824d8b2b0e4f0dc5885e46e2eaf335
Joni Ahola <joni@juotos.fi>
2014-10-03 13:35:51 +0300

    Clean read_file file. Now user can specify filename which program parse data and create categories and recipes

--- read_file.py
+++ read_file.py
@@ -6,26 +6,41 @@ import re
 
 """
 Just for reading recipes quick in txt file
+Syntax in your recipe file:
+<Category>
+Recipename1
+Recipename2
+<Category2>
+Recipemane3
+...
+
+Example:
+<Fish>
+Fish and Chips
+Fish soup
+<Pork>
+Pulled pork
+...
 """
-def save_to_file(recept):
-	old_data = get_recipes_data()
-	old_data["recipes"].append(recept.save_data_to_file())
-	write_json_to_file(old_data, filenames["recipes"])
 
-regex = '<.*>'
-
-f = open('ruuat.txt', 'r')
-
+regex_for_category = '<.*>'
 category = ""
 counter = 0
+
+try:
+	filename = read_input("Anna tiedostonimi päätteineen. Jos tiedosto ei sijaitse samassa kansiossa, absoluuttinen polku. Esim: ruuat.txt")
+	f = open(filename, 'r')
+except:
+	print "Tiedostoa ei löydy tai jokin muu meni vikaan"
+	raise
 
 for line in f:
 	l = line.rstrip('\n')
 	if not l: continue
-	if re.search(regex, l):
+	if re.search(regex_for_category, l):
 		category = l.strip('<>')			
 	else:
-		recept = Recipe(l, category, "")
+		recept = Recipe(l, category)
 		save_to_file(recept)
 		counter += 1
 
