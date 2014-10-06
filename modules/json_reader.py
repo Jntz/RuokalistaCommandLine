9170e6fe97616fbc5e79dc493c8e73e6e4b69343
Joni Ahola <joni@juotos.fi>
2014-10-06 13:10:15 +0300

    Add try-catch blocks to read_json_from_file function

--- modules/json_reader.py
+++ modules/json_reader.py
@@ -2,10 +2,10 @@
 import json, io
 
 def read_json_from_file(filename):
-	with open(filename, 'r') as f:
-		read_data = f.read()
-	f.closed
 	try:
+		with open(filename, 'r') as f:
+			read_data = f.read()
+		f.closed
 		json_data = json.loads(read_data)
 	except:
 		json_data = None
