67ddf94f53422066c49249225390587663b58941
Joni Ahola <joni@juotos.fi>
2014-10-06 13:07:05 +0300

    Add menus_reader module

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,5 +2,6 @@ from input_reader import *
 from json_reader import *
 from json_writer import *
 from recipes_reader import *
+from menus_reader import *
 from helpers import *
 from config import *
---
+++ modules/menus_reader.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from json_reader import *
+from config import *
+
+def get_menus_data():
+	old_data = read_json_from_file(filenames["menus"])
+	if old_data == None or type(old_data) is not dict: # rewrite old_data and create new recipe dictionary
+		# initialize new dict
+		old_data = {}
+		old_data["menus"] = {}
+	elif "menus" not in old_data and type(old_data) is dict: # save other data (maybe worthless)
+		# add new row: recipes
+		old_data["menus"] = {}
+	return old_data
+def get_menus(): 
+	data = get_menus_data()
+	return data["menus"]
+
+def get_menu(index): #get recipe with spesific index
+	pass
+	#return get_menus()[index]
