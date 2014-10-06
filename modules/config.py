27512b35a11371c8b871a87926ca14618ba4dc4f
Joni Ahola <joni@juotos.fi>
2014-10-06 13:08:48 +0300

    Add new config value: reuse_weeks (meaning that how many weak takes that you can reuse recipe)

--- modules/config.py
+++ modules/config.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 project_name = 'Ruokalista Generaattori'
 filenames = {"recipes": 'recipes.json', "menus": 'menus.json'}
+reuse_weeks = 4	# how many weeks need to take that recept can use
